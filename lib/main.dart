9745f6c127b0cf2a7cc86f297695b322eaec67f3
Deam <deam@aleph.engineering>
2022-09-17 18:27:07 -0400

    Use custom widgets increasing code readability

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:devcamp/core/constants.dart';
-import 'package:devcamp/data/sources.dart';
-import 'package:devcamp/widgets/card.dart';
-import 'package:devcamp/widgets/tag.dart';
+import 'package:devcamp/widgets/grid_list.dart';
+import 'package:devcamp/widgets/tag_list.dart';
+import 'package:devcamp/widgets/top_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -40,29 +39,10 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 350,
-              foregroundDecoration: BoxDecoration(
-                gradient: whiteBottomGradient,
-              ),
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                      'https://img.freepik.com/free-photo/full-shot-travel-concept-with-landmarks_23-2149153258.jpg?3&w=2000&t=st=1663413910~exp=1663414510~hmac=6ee180449b95322d08aa5ff2557fe0d0be39c0bed3cad8b52232c68de06c47cc'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                Tag(text: 'Places'),
-                Tag(text: 'Hotels'),
-                Tag(text: 'Attractions'),
-              ],
-            ),
-            const Padding(
+          children: const [
+            TopImageWidget(),
+            TagListWidget(),
+            Padding(
               padding: EdgeInsets.symmetric(horizontal: 20),
               child: Text(
                 'Popular Destinations',
@@ -72,28 +52,10 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(
+            SizedBox(
               height: 20,
             ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 25,
-                  crossAxisSpacing: 25,
-                ),
-                itemCount: cardItems.length,
-                itemBuilder: (context, index) {
-                  return CardWidget(
-                    title: cardItems[index].title,
-                    image: cardItems[index].image,
-                  );
-                },
-              ),
-            ),
+            GridListWidget(),
           ],
         ),
       ),
