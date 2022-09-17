49623ffe1df9b7bc91a8677d316169867ce4258a
Deam <deam@aleph.engineering>
2022-09-17 13:53:00 -0400

    Add card title widget

---
+++ lib/widgets/card_title.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../core/constants.dart';
+
+class CardTitleWidget extends StatelessWidget {
+  const CardTitleWidget({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: whiteTopGradient,
+      ),
+      child: Center(
+        child: Text(
+          title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
