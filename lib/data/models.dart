2d9191e9b7d6bd227d3346d57470947d108f9ed7
Deam <deam@aleph.engineering>
2022-09-24 17:16:04 -0400

    Add places and description to card item model

--- lib/data/models.dart
+++ lib/data/models.dart
@@ -2,8 +2,24 @@ class CardItem {
   CardItem({
     required this.title,
     required this.image,
+    required this.description,
+    required this.places,
   });
 
   final String title;
   final String image;
+  final String description;
+  final List<Place> places;
+}
+
+class Place {
+  Place({
+    required this.name,
+    required this.image,
+    required this.description,
+  });
+
+  final String image;
+  final String name;
+  final String description;
 }
